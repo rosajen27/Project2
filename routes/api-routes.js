32ac17af0218d94c27e4e8966c0ae5d5a793884a
Felix <bpr59@comcast.net>
2020-07-31 08:08:36 -0400

    Update api file
    updated file to add handlebars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,14 @@
 // Dependencies
+const db = require("../models");
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
-var db = require("../models");
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 module.exports = function(app) {
