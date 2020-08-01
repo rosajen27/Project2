0bb4e94847c09e7104de9a7eeb41f1769c1ac057
Jennifer Rosa <rosajen27@gmail.com>
2020-08-01 11:30:26 -0400

    display results of search

--- public/js/search.js
+++ public/js/search.js
@@ -18,13 +18,21 @@ $(document).ready(function () {
         // hit the queryURL with $ajax, response will return an array with artist matching searched artist name
         var queryURL = "https://itunes.apple.com/search?term=" + artist + "&entity"
 
+        // ajax get request
         $.ajax({
             url: queryURL,
+            type: 'post',
             method: "GET",
+            dataType: 'json',
         }).then(function (data) {
-            console.log(data);
 
-            // display list of songs
+
+            let results = data.results;
+            console.log("DATA.RESULTS: " + results);
+
+                $.each(results, function(index, results){
+                    $("#well-section").append("<br><br> <img src='" + results.artworkUrl100 + "'> <b>Artist: </b>" + results.artistName + " | <b>Song : </b> <a target='_blank' href='" + results.previewUrl + "'>" + results.trackName + "</a>" + " | <b>Album: </b>" + results.collectionName + " | <b>Genre: </b>" + results.primaryGenreName + "<br><br><hr><br><br>");
+                });
+            });
         });
     });
-});
