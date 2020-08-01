ecc40cd6781573b7fbee8736ab6a4fdedf3264e4
Felix <bpr59@comcast.net>
2020-07-31 22:10:16 -0400

    Update server.js
    Edited vars and app not needed in this file.

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-// Dependencies
+// *** Dependencies
 var express = require("express");
-var http = require("http");
 
 // Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-
-// Start our server so that it can begin listening to client requests.
-
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
 db.sequelize.sync().then(function() {
-      // Log (server-side) when our server has started
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
-})
-
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on PORT: " + PORT);
-});
-
