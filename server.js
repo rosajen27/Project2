8e35ff9d6606c6d8b938b4f581f32c3f7e714cb2
Felix <bpr59@comcast.net>
2020-08-07 20:52:15 -0400

    update to the server.js file

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-// *** Dependencies
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const methodOverride = require('method-override');
+const mysql = require("mysql");
+const path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const routes = require("./routes/handlers")
 
-// Requiring our models for syncing
-var db = require("./models");
+const PORT = process.env.PORT || 8080;
+const app = express();
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
 
+app.use(express.static("public"));
 
-// Static directory
-app.use(express.static('public'));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+app.use("/", routes);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
