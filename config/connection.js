3640ca0a947342276838294d7bfa3efa5b42a81b
Jennifer Rosa <rosajen27@gmail.com>
2020-08-19 19:56:04 -0400

    set up JAWS DB for Heroku Deployment

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -9,5 +13,4 @@ let connection = mysql.createConnection({
   });
 
   connection.connect();
-
   module.exports = connection;
