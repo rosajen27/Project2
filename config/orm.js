97b9192003a080ee8a360b088abaf879dcf870f3
Jennifer Rosa <rosajen27@gmail.com>
2020-08-19 21:17:42 -0400

    linting

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,34 @@
 const connection = require("./connection");
 
 const orm = {
-    selectAll: function(cb) {
-       connection.query("SELECT * FROM playlist", function (err, data){
-           if (err) cb (err, null);
-           cb(null, data);
-       }) 
-    },
+	selectAll: function(cb) {
+		connection.query("SELECT * FROM playlist", function (err, data){
+			if (err) {
+				cb (err, null);
+			}
+			cb(null, data);
+		});
+	},
 
-    insertOne: function (newSong, cb) {
-        const sqlQuery = `INSERT INTO playlist(artist_name, song_name) VALUES('${newSong}')`;
-        connection.query(sqlQuery, function (err, data){
-                if (err) cb(err, null)
-                cb(null, data);
-        });
-    },
+	insertOne: function (newSong, cb) {
+		const sqlQuery = `INSERT INTO playlist(artist_name, song_name) VALUES('${newSong}')`;
+		connection.query(sqlQuery, function (err, data){
+			if (err) {
+				cb(err, null);
+			}
+			cb(null, data);
+		});
+	},
 
-    deleteOne: function (id, cb) {
-        const sqlQuery = `DELETE FROM playlist WHERE id = ${id}`;
-        connection.query(sqlQuery, function (err, data){
-                if (err) cb(err, null)
-                cb(null, data);
-        });
-    }
+	deleteOne: function (id, cb) {
+		const sqlQuery = `DELETE FROM playlist WHERE id = ${id}`;
+		connection.query(sqlQuery, function (err, data){
+			if (err) {
+				cb(err, null);
+			}
+			cb(null, data);
+		});
+	}
 
 };
 
--- public/js/search.js
+++ public/js/search.js
@@ -1,19 +1,19 @@
 (function (window, document, undefined) {
-    'use strict';
-    var searchButton = document.getElementById('searchButton');
-    var searchInput = document.getElementById('searchInput');
-    // when user clicks on button, we want to call function start search
-    searchButton.addEventListener('click', startSearch);
-    function startSearch(event) {
-        event.preventDefault();
-        var inputValue = document.getElementById('searchInput').value;
-        var urlBase = `${window.location.origin}`;
-        if (inputValue === null || inputValue === '') {
-            searchInput.classList.add('err');
-            return;
-        }
-        var searchUrl = `${urlBase}/results?term=${inputValue}&media=music`;
-        console.log(searchUrl);
-        window.location.replace(searchUrl);
-    }
+	"use strict";
+	var searchButton = document.getElementById("searchButton");
+	var searchInput = document.getElementById("searchInput");
+	// when user clicks on button, we want to call function start search
+	searchButton.addEventListener("click", startSearch);
+	function startSearch(event) {
+		event.preventDefault();
+		var inputValue = document.getElementById("searchInput").value;
+		var urlBase = `${window.location.origin}`;
+		if (inputValue === null || inputValue === "") {
+			searchInput.classList.add("err");
+			return;
+		}
+		var searchUrl = `${urlBase}/results?term=${inputValue}&media=music`;
+		console.log(searchUrl);
+		window.location.replace(searchUrl);
+	}
 })(window, document);
