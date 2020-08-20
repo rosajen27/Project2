bc49fd4c15421277c5d1de97cfd7a77feab584d2
Jennifer Rosa <rosajen27@gmail.com>
2020-08-19 20:13:12 -0400

    fixed JawsDB code

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql');
-
 var connection;
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "songster_db"
   });
+};
 
   connection.connect();
+
   module.exports = connection;
